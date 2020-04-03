e308015c03bb9cfdd2e27bf25078d5f4b3ba087b
mohamedhammam439 <mohamedhammam439@gmail.com>
2020-04-03 12:18:41 +0200

    Updates

---
+++ precache-manifest.aa00d12134e650d2d1efa7658befc6ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570cbbcb5adb60d34e2c400339979423",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "1e48379c77343e3d7a1d",
+    "url": "/React-App/static/css/main.4cd60ef1.chunk.css"
+  },
+  {
+    "revision": "48dccac2552715ef388e",
+    "url": "/React-App/static/js/2.59dee93b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-App/static/js/2.59dee93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e48379c77343e3d7a1d",
+    "url": "/React-App/static/js/main.874f084b.chunk.js"
+  },
+  {
+    "revision": "bf71ae581ca0b64eee14",
+    "url": "/React-App/static/js/runtime-main.cca162ed.js"
+  },
+  {
+    "revision": "71156fd04130a64af8f40fea6bd6943c",
+    "url": "/React-App/static/media/1.71156fd0.jpg"
+  },
+  {
+    "revision": "89ccac5365f50ee229092a997f5e62ca",
+    "url": "/React-App/static/media/2.89ccac53.jpg"
+  },
+  {
+    "revision": "8baf47ae97b1394757b21f7d86472409",
+    "url": "/React-App/static/media/3.8baf47ae.jpg"
+  },
+  {
+    "revision": "31a0178ae7aaff8ee2f0fa0f7de768a4",
+    "url": "/React-App/static/media/4.31a0178a.jpg"
+  }
+]);
